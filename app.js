6e142dfd748da03a271ddd7e2e835542158a2c7d
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-05-24 22:40:01 -0300

    not-found view and minor changes

--- app.js
+++ app.js
@@ -48,22 +48,33 @@ app.get('/projects/:id', (req, res) => {
 });
 
 /**
- * ERROR HANDLERS 
+ * ERROR HANDLERS - reference https://teamtreehouse.com/library/practice-error-handling-in-express
  */
 
+/* 404 handler to catch undefined or non-existent route requests */ 
 app.use((req, res, next) => {
-  const err = new Error('Not Found :(');
-  err.status = 404;
-  next(err);
+
+  console.log('404 error');
+  res.status(404).render('not-found');
 });
 
+/* Global error handler */
 app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render('error');
+
+  if (err) {
+    console.log('Global error handler called', err);
+  }
+ /**
+  * Set the response status to 404
+  * Render the 'not-found' view and pass the error object to the view
+  */
+  if (err.status === 404) {
+    res.status(404).render('not-found', { err });
+  } else {
+    err.message = err.message || `Oops!  It looks like something went wrong on the server.`;
+    res.status(err.status || 500).render('error', { err }); //Render the 'error' view, passing it the error object
+  }
 });
-
-
 
 /* PORT */
 app.listen(3000, () => {
